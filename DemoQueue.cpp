a5351e03c43fd28f60d55f93b789a448ee220948
Alexis <alogh11032002@gmail.com>
2026-02-12 10:56:45 -0500

    Cola completada

---
+++ DemoQueue.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include "containers/queue.h"
+#include "variadic-util.h"
+
+using namespace std;
+
+void DemoQueue(){
+    CQueue<T1> Q1;
+
+    Q1.Push(4);
+    Q1.Push(1);
+    Q1.Push(8);
+    Q1.Push(3);
+    cout << Q1 << endl;
+
+    ifstream inf("queue.txt");
+    CQueue<T1> Q2;
+    inf >> Q2;
+    inf.close();
+    cout << "Cola leida desde archivo:" << endl;
+    cout << Q2 << endl;
+
+    cout << endl << "Front y Pop" << endl;
+    cout << "Front actual: " << Q1.Front() << endl;
+    Q1.Pop();
+    cout << "Despues de Pop: " << Q1 << endl;
+    cout << "Nuevo Front: " << Q1.Front() << endl;
+
+    cout << endl << "PROBANDO CONSTRUCTOR COPIA" << endl;
+    CQueue<T1> Q3 = Q1;
+    cout << Q3 << endl;
+    cout << endl;
+
+    cout << "PROBANDO OPERADOR '='" << endl;
+    CQueue<T1> Q4;
+    Q4.Push(99);
+    cout << endl;
+    cout << "Cola 4" << endl;
+    cout << Q4 << endl;
+    cout << endl;
+    Q4 = Q1;
+    cout << "Cola 4 modificada" << endl;
+    cout << Q4 << endl;
+    cout << endl;
+
+    cout << endl;
+    cout << "MOVE CONSTRUCTOR - cola 5" << endl;
+    CQueue<T1> Q5 = std::move(Q3);
+    cout << Q5 << endl;
+
+    cout << endl;
+    cout << "Operador '=' en move - cola 6" << endl;
+    CQueue<T1> Q6;
+    Q6 = std::move(Q4);
+    cout << Q6 << endl;
+}
--- containers/queue.h
+++ containers/queue.h
@@ -2,12 +2,201 @@
 #define __QUEUE_H__
 
 #include <iostream>
+#include <mutex>
+#include <cassert>
+#include <utility>
 #include "../general/types.h"
 #include "../util.h"
 
-class CQueue{
+using namespace std;
+
+template  <typename T>
+class NodeQueue{
+    using value_type = T;
+private:
+    value_type m_data;
+    NodeQueue* m_pNext = nullptr;
+public:
+    NodeQueue(){};
+    NodeQueue(value_type _value, NodeQueue* _next )
+        : m_data(_value) , m_pNext(_next)
+    {}
+    value_type  GetValue    () const { return m_data;  }
+    value_type &GetValueRef ()       { return m_data;  }
+    NodeQueue*   GetNext     () const { return m_pNext; }
+    NodeQueue*  &GetNextRef  ()       { return m_pNext; }
     
 };
 
+template <typename T>
+class CQueue{
+    using NodeQueue  = NodeQueue<T>;
+    using value_type = T;
+private:
+    NodeQueue* m_pFront = nullptr;
+    NodeQueue* m_pRear  = nullptr;
+    size_t m_nElements = 0;
+    mutable std::recursive_mutex m_mtx;
 
+public:
+    void Push(const value_type& value);
+    void Pop();
+    value_type& Front();
+    const value_type& Front() const;
+    CQueue(){}
+    //Copy constructor
+    CQueue(const CQueue<T>& other);
+    //Move constructor
+    CQueue(CQueue<T>&& other) noexcept 
+        : m_pFront    (std::exchange(other.m_pFront, nullptr)),
+          m_pRear     (std::exchange(other.m_pRear, nullptr)),
+          m_nElements (std::exchange(other.m_nElements, 0))
+        {}
+    //Destructor Seguro
+    virtual ~CQueue();
+    //Operador '=' copia
+    CQueue& operator=(const CQueue<T>& other);
+    //Operador '=' robado
+    CQueue& operator=(CQueue<T>&& other) noexcept;
+
+private:
+    //Clear para limpiar colas
+    void Clear();
+    void CopyFrom(const CQueue<T>& other);
+    friend ostream& operator<<(ostream& os , CQueue<T>& queue){
+        std::lock_guard<std::recursive_mutex> lock(queue.m_mtx);
+        os << "CQueue: size = " << queue.m_nElements << std::endl;
+        NodeQueue* p_CurrentNode = queue.m_pFront;
+        while(p_CurrentNode){
+            os<< "[" << p_CurrentNode->GetValue() << "]";
+            p_CurrentNode = p_CurrentNode->GetNext();
+            if (p_CurrentNode)
+                os << "->";
+        }
+        return os;
+    }
+    friend istream& operator>>(istream& is , CQueue<T>& queue){
+        size_t nElements;
+        is >> nElements;
+        for (size_t i=0 ; i < nElements ; ++i){
+            value_type val;
+            is >> val;
+            queue.Push(val);
+        }
+        return is;
+    }
+};
+
+template <typename T>
+void CQueue<T>::Clear(){
+    NodeQueue* p_CurrentNode = m_pFront;
+    while (p_CurrentNode){
+        NodeQueue* p_nextNode = p_CurrentNode->GetNext();
+        delete p_CurrentNode;
+        p_CurrentNode = p_nextNode;
+    }
+    m_pFront    = nullptr;
+    m_pRear     = nullptr;
+    m_nElements = 0; 
+}
+
+template <typename T>
+void CQueue<T>::CopyFrom(const CQueue<T>& other){
+    NodeQueue* p_CurrentNode = other.m_pFront;
+    while(p_CurrentNode){
+        NodeQueue* pNew = new NodeQueue(p_CurrentNode->GetValue(), nullptr);
+        if(!m_pFront)
+            m_pFront = pNew;
+        else
+            m_pRear->GetNextRef() = pNew;
+
+        m_pRear = pNew;
+        ++m_nElements;
+        p_CurrentNode = p_CurrentNode->GetNext();
+    }
+}
+
+
+//agregar al final de la cola
+template <typename T>
+void CQueue<T>::Push(const typename CQueue::value_type& value){
+    std::lock_guard<std::recursive_mutex> lock(m_mtx);
+    NodeQueue* pNew = new NodeQueue ( value, nullptr );
+    if(!m_pFront){
+        m_pFront = pNew;
+        m_pRear  = pNew;
+    } else {
+        m_pRear->GetNextRef() = pNew;
+        m_pRear = pNew;
+    }
+    ++m_nElements;
+}
+
+//quitar del inicio de la cola
+template <typename T>
+void CQueue<T>::Pop(){
+    std::lock_guard<std::recursive_mutex> lock(m_mtx);
+    assert(m_pFront);
+    NodeQueue* p_nextNode = m_pFront->GetNext();
+    delete m_pFront;
+    m_pFront = p_nextNode;
+    if(!m_pFront)
+        m_pRear = nullptr;
+    --m_nElements;
+}
+
+//ver el primero con posibilidad de modificar
+template <typename T>
+typename CQueue<T>::value_type& CQueue<T>::Front(){
+    std::lock_guard<std::recursive_mutex> lock(m_mtx);
+    assert(m_pFront);
+    return m_pFront->GetValueRef(); 
+} 
+//solo leer el primero de la cola
+template <typename T>
+const typename CQueue<T>::value_type& CQueue<T>::Front() const{
+    std::lock_guard<std::recursive_mutex> lock(m_mtx);
+    assert(m_pFront);
+    return m_pFront->GetValue(); 
+} 
+
+// Copy constructor
+template <typename T>
+CQueue<T>::CQueue(const CQueue<T>& other){
+    std::lock_guard<std::recursive_mutex> lock(other.m_mtx);
+    CopyFrom(other);
+}
+
+//Destructor seguro
+template <typename T>
+CQueue<T>::~CQueue(){
+   Clear();
+}
+
+//Operador '=' - Copy  
+template <typename T>
+CQueue<T>& CQueue<T>::operator=(const CQueue<T>& other){
+    if (this == &other)
+        return *this;
+    std::lock(m_mtx,other.m_mtx);
+    std::lock_guard<std::recursive_mutex> lock1(m_mtx, std::adopt_lock);
+    std::lock_guard<std::recursive_mutex> lock2(other.m_mtx, std::adopt_lock);
+    Clear();
+    CopyFrom(other);
+    return *this;
+}
+
+//Operador '=' - Move 
+template <typename T>
+CQueue<T>& CQueue<T>::operator=(CQueue<T>&& other) noexcept{
+    if (this == &other)
+        return *this;
+    std::lock_guard<std::recursive_mutex> lock1(m_mtx);
+    std::lock_guard<std::recursive_mutex> lock2(other.m_mtx);
+    Clear();
+    m_pFront    = std::exchange (other.m_pFront,nullptr);
+    m_pRear     = std::exchange (other.m_pRear,nullptr);
+    m_nElements = std::exchange (other.m_nElements,0);
+    return *this;
+}
 #endif // __QUEUE_H__
